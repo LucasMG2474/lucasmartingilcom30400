67cc061247dba6af7243b60469738b383b89e995
lucas gil <lukemgl@outlook.com>
2022-06-23 16:01:26 -0300

    deseafio incorporar eventos

---
+++ js/stock.js
@@ -0,0 +1,15 @@
+const stockProductos = [
+  {id: 1, nombre: 'amd', precio: 23000, img: '../images/amd.jpg'},
+  {id: 2, nombre: 'intel', precio: 21000, img: '../images/intel.jpg'}, 
+  {id: 3, nombre: 'asus', precio: 14000, img: '../images/asus.jpg'},
+  {id: 4, nombre: 'gigabyte', precio: 8400, img: '../images/giga.jpg'},
+  {id: 5, nombre: 'kingston', precio: 7000, img: '../images/ram.jpg'},
+  {id: 6, nombre: 'corsair', precio: 5000, img: '../images/ram.jpg'},
+  {id: 7, nombre: 'hd', precio: 5600, img: '../images/hd.jpg'},
+  {id: 8, nombre: 'ssd', precio: 6500, img: '../images/ssd.jpg'},
+  {id: 9, nombre: 'sentey', precio: 5000, img: '../images/sentey.jpg'},
+  {id: 10, nombre: 'aerocool', precio: 6000, img: '../images/aerocool.jpg'},
+  {id: 11, nombre: 'teclado', precio: 2500, img: '../images/teclado.jpg'},
+  {id: 12, nombre: 'monitor', precio: 30000, img: '../images/moni.jpg'},
+  {id: 13, nombre: 'mouse', precio: 2500, img: '../images/mouse.jpg'}
+]
